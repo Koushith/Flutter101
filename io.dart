986ea67c609e7893e18da4449c2697657f221a8f
koushith <koushith97@gmail.com>
2020-01-02 01:49:16 +0530

    comments added

--- io.dart
+++ io.dart
@@ -1,9 +1,17 @@
 //user input, for any i/o operations we have to import io lib
-
 import 'dart:io';
 
 main() {
-  stdout.writeln("What is your Name?");
-  String name = stdin.readLineSync();
+  stdout.writeln("What is your Name?"); //Accepts user input
+  String name = stdin.readLineSync(); //waits for user inputs
   print('My name is $name');
 }
+
+//Inline comment
+
+/*
+Block comments,
+Multiline comments
+*/
+
+//Docs
