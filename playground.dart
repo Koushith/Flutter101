3224fba041f1a7a9a7cb8bf28e213b46aa7946b9
koushith <koushith97@gmail.com>
2020-01-02 01:26:07 +0530

    Hello Alieans

--- playground.dart
+++ playground.dart
@@ -1,3 +1,8 @@
+// can be used with void or not this will not return anything
+
 main() {
-  print("Hello World")
+  var FirstName =
+      "koushith"; //Type inference: Automatically detects what type it is.
+  String LastName = "Amin"; // Statically Defined
+  print(FirstName + " " + LastName);
 }
