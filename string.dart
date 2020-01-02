a7573252d74f57148a9902ca96ae075cf24c23de
koushith <koushith97@gmail.com>
2020-01-03 05:19:05 +0530

    strings

---
+++ string.dart
@@ -0,0 +1,13 @@
+//Strings
+
+import 'dart:io';
+
+main() {
+  var s1 = 'Single Quotes';
+  var s2 = "Double Quotes";
+  var s3 =
+      'It\'s okay to escape'; // use backslash whenever you want to print 's
+  print(s1);
+  print(s2);
+  print(s3);
+}
