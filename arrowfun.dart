0fd18829beb262bffeffb97f6ed8131669d71466
koushith <koushith97@gmail.com>
2020-01-05 22:17:30 +0530

    Greet

--- arrowfun.dart
+++ arrowfun.dart
@@ -1,9 +1,11 @@
 //Arrow Function
 
 void main() {
+  var name = "koushith";
   print(sum(2));
+  var greet();
 }
 
-dynamic sum(var n) {
-  return n + n;
-}
+dynamic sum(var n) => n + n;
+
+String greets(String msg) => "Hello ";
