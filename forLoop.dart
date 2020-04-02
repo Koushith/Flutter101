764e70ab00856f8414dde8a112ac81d4ae8c9232
koushith <koushith97@gmail.com>
2020-04-02 23:09:57 +0530

    for

--- forLoop.dart
+++ forLoop.dart
@@ -4,11 +4,11 @@ main() {
   //standard loop like other lang
 
   //For in --> inspired by python
-  var numbers = [1, 2, 3];
-  for (var n in numbers) {
+  var number = [1, 2, 3];
+  for (var n in number) {
     print(n); //Prints all numbers
   }
   for (var i = 0; i < numbers.length; i++) {
     print(numbers[2]);
   }
-}
+};
